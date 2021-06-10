cce33ea84dd17e54c9de3f0630a88c9b28abdb1b
EduardoGonzalezParra <eduardogp2203@gmail.com>
2021-06-09 19:48:59 -0500

    Ejercicio 6 - Tarea completa

---
+++ src/main/java/uaslp/objetos/escuela/Dependencia3.java
@@ -0,0 +1,7 @@
+package uaslp.objetos.escuela;
+
+public interface Dependencia3 {
+    default void send(int a, int b) {};
+
+    default void recover() {};
+}
--- src/test/java/uaslp/objetos/escuela/Exercise6.java
+++ src/test/java/uaslp/objetos/escuela/Exercise6.java
@@ -1,6 +1,7 @@
 package uaslp.objetos.escuela;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -24,5 +25,71 @@ public class Exercise6 {
         assertThat(Dependencia1.class).isInterface();
         assertThat(Dependencia2.class).isInterface();
         assertThat(Dependencia3.class).isInterface();
+    }
+
+    @Test
+    public void validarAEqualsB(){
+        //Given
+        int a = 5;
+        int b = 5;
+        String c = "Guardado";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1, dependencia2, dependencia3);
+
+        //When
+        algoritmoX.algoritmoACubrir(5, 5, "Guardado");
+
+        //Then
+        Mockito.verify(dependencia1).save(c);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1, dependencia2, dependencia3);
+    }
+
+    @Test
+    public void validarALowerThanB(){
+        //Given
+        int a = 3;
+        int b = 5;
+        String c = "Impreso";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1, dependencia2, dependencia3);
+
+        //When
+        algoritmoX.algoritmoACubrir(3, 5, "Impreso");
+
+        //Then
+        Mockito.verify(dependencia2).print(a, c);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1, dependencia2, dependencia3);
+    }
+
+    @Test
+    public void validarAHigherThanB(){
+        //Given
+        int a = 5;
+        int b = 3;
+        String c = "Enviado";
+
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+
+        AlgoritmoX algoritmoX = new AlgoritmoX(dependencia1, dependencia2, dependencia3);
+
+        //When
+        algoritmoX.algoritmoACubrir(5, 3, "Enviado");
+
+        //Then
+        Mockito.verify(dependencia3).send(a, b);
+        Mockito.verify(dependencia3).recover();
+        Mockito.verifyNoMoreInteractions(dependencia1, dependencia2, dependencia3);
     }
 }
